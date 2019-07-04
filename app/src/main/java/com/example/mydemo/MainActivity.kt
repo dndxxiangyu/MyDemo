2465f5a5249e26224903af200362f8fbf11d5fcb
wuxiangyu <wuxaingyu.lc@bytedance.com>
2019-07-04 11:03:19 +0800

    feat: 静态代码检查

--- app/src/main/java/com/example/mydemo/MainActivity.kt
+++ app/src/main/java/com/example/mydemo/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.mydemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * MainActivity is missing required documentation.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/mydemo/util/AppUtil.kt
+++ app/src/main/java/com/example/mydemo/util/AppUtil.kt
@@ -2,7 +2,13 @@ package com.example.mydemo.util
 
 import android.content.Context
 
+/**
+ * AppUtil is missing required documentation.
+ */
 object AppUtil {
+    /**
+     * The function getScreenHeight is missing documentation.
+     */
     fun getScreenHeight(context: Context): Int {
         return context.resources.displayMetrics.heightPixels
     }
